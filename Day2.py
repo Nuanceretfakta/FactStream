038f133a371b2a6024ab1de641a30f39d45bfd86
Nuanceretfakta <Gaston_954@hotmail.com>
2025-11-26 14:46:42 +0100

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,10 @@
+# Day 2 – Input og beregning
+name = input("AleGS:")
+age = int(input("Alder: "))
+
+years_to_40 = 40 - age
+
+print("Hej " + name + "!")
+print("Du er " + str(age) + " år gammel.")
+print("Du har " + str(years_to_40) + " år til du er 40.")
+
