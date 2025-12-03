76d989d508b602c7b4456fed7bbb26f227710c9c
Nuanceretfakta <Gaston_954@hotmail.com>
2025-12-03 13:35:36 +0100

    Add files via upload

---
+++ Day8 - Dictionaries + loops.py
@@ -0,0 +1,10 @@
+print("Learning AI – Day 8")
+
+personer = {
+    "Maria": 24,
+    "Jonas": 30,
+    "AleGS": 27
+}
+
+for navn, alder in personer.items():
+    print(navn + " er " + str(alder) + " år gammel.")
