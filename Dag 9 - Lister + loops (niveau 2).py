d7190588e886b3db8a7244b86609fbbdd312e9bb
Nuanceretfakta <Gaston_954@hotmail.com>
2025-12-04 14:35:03 +0100

    Add files via upload

---
+++ Dag 9 - Lister + loops (niveau 2).py
@@ -0,0 +1,16 @@
+print("Learning AI – Day 9")
+
+# Første opgave: navne
+navne = ["Maria", "Jonas", "AleGS", "Sofie", "Lukas"]
+
+for navn in navne:
+    print("Hej " + navn + " – håber du har en god dag!")
+
+print()
+
+
+tal = [3, 7, 12, 20, 45]
+
+for nummer in tal:
+    nyt_tal = nummer + 5
+    print("Originalt tal:", nummer, "→ Nyt tal:", nyt_tal)
