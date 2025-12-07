f1f98abe285f703b4177eb06c07a921d515e104f
Nuanceretfakta <Gaston_954@hotmail.com>
2025-12-07 10:17:40 +0100

    Add files via upload

---
+++ Day 11- If:Else-logik i funktioner. py.py
@@ -0,0 +1,14 @@
+print("Learning AI – Day 11")
+
+def vurder_tal(tal):
+    if tal > 10:
+        print("Tallet er stort.")
+    elif tal >= 5:
+        print("Tallet er medium.")
+    else:
+        print("Tallet er lille.")
+
+# Test funktionen
+vurder_tal(12)
+vurder_tal(7)
+vurder_tal(3)
